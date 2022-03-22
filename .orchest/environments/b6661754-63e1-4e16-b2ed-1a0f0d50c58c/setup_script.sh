7828300ea1790a45a86401cc783720e583789c49
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-03-22 15:07:20 +0100

    update to k8s orchest

--- .orchest/environments/b6661754-63e1-4e16-b2ed-1a0f0d50c58c/setup_script.sh
+++ .orchest/environments/b6661754-63e1-4e16-b2ed-1a0f0d50c58c/setup_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-pip install dbt-postgres pyyaml psycopg2-binary pandas SQLAlchemy
+sudo apt update
+sudo apt install git -y
+pip install dbt-postgres pyyaml psycopg2-binary pandas SQLAlchemy matplotlib
